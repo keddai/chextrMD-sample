d4042713da7ff90173c9fd9b9f14e69ed848d3aa
Dai <85894802+keddai@users.noreply.github.com>
2024-01-06 21:11:42 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,53 +1,45 @@
 // RESONSIVE NAVBAR | CODE REFERENCED FROM WEB DEV SIMPLIFIED
 //Getting hamburger menu and list element by class name
-const hamburger_menu = document.getElementsByClassName('hamburger_button')[0]
-const navbar_links = document.getElementsByClassName('navbar_links')[0]
+// const nav_toggle = document.querySelector('nav_toggle');
+// const nav_list = document.querySelector('nav_list');
 
-//Toggle function to show nav_list
-hamburger_menu.addEventListener('click', () => {
-    navbar_links.classList.toggle('active')
-})
+// //Toggle function to show nav_list
+// nav_toggle.addEventListener('click', () => {
+//   nav_toggle.classList.toggle("active");
+//   nav_list.classList.toggle("active");
+// })
 
-// FOOTER COPY TO CLIPBOARD FUNCTION | CODE REFERENCED FROM W3SCHOOLS
-function copyPhone() {
-    let text = document.getElementById("copy-phone").innerHTML;
-    navigator.clipboard.writeText(text);
+const navigation = document.querySelector(".navigation");
+const navToggle = document.querySelector(".nav_toggle");
 
-    var tooltip = document.getElementsByClassName("tooltiptext")[0]
-    tooltip.innerHTML = "Copied";
-}
+navToggle.addEventListener("click", () => {
+  const visibilty = navigation.getAttribute("data-visible");
+  if (visibilty === "false") {
+    navToggle.classList.toggle("active");
+    navigation.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true)
+  } else if (visibilty === "true") {
+    navToggle.classList.toggle("active");
+    navigation.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false)
+  }
+});
 
-function copyEmail() {
-    let text = document.getElementById("copy-email").innerHTML;
-    navigator.clipboard.writeText(text);
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-    var tooltip = document.getElementsByClassName("tooltiptext")[1]
-    tooltip.innerHTML = "Copied";
-}
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
 
-function reTooltip() {
-    var tooltip = document.getElementsByClassName("tooltiptext");
-    console.log(tooltip[0])
-
-    for (var i = 0; i < tooltip.length; i++) {
-        tooltip[i].innerHTML = "Copy";
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
+  });
 }
-
-// Initialize and add the map
-function initMap() {
-    // The location of business
-    const loc = { lat: -25.344, lng: 131.031 };
-    // The map, centered at business
-    const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: loc,
-    });
-    // The marker, positioned at business
-    const marker = new google.maps.Marker({
-    position: loc,
-    map: map,
-    });
-}
-
-window.initMap = initMap;
